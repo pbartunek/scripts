cee904bbdd25997d0aa822a9e45fb75bcc2dc095
Pawel <pawel.bartunek@gmail.com>
2019-08-21 11:15:21 +0200

    refactoring

--- nmap-scan.sh
+++ nmap-scan.sh
@@ -5,6 +5,13 @@
 
 quickPorts="21,22,23,80,111,443,445,1080,1098,1099,1198,1199,1414,1433,1521,2375,3306,3389,4443,4444,5555,5800,5900,6000,6379,6600,7001,7002,7202,8000,8009,8080,8081,8042,8082,8088,8090,8089,8181,8282,8443,8888,9000,9001,9043,9090,9443,9700,9494,9797,9800,9875,9999,10090,10099,11098,11099,11111,11112"
 httpPorts="80,443,3000,5985,8000,8080,8081,8181,8282,8443,8880,9000,9090,9443"
+
+httpScripts="--script=http-title.nse --script=http-userdir-enum.nse \
+        --script=http-headers.nse --script=http-methods.nse \
+        --script=http-apache-server-status.nse --script=http-git.nse \
+        --script=http-ls.nse --script=http-robots.txt.nse \
+        --script http-wordpress-enum --script-args check-latest=true,search-limit=10"
+
 nmapFlagsServices=" -sTV --version-all -A "
 nmapFlags="-vvv --open"
 scanType=""
@@ -38,12 +45,7 @@ while getopts ":sfqvt:T:Hr:" opt; do
       ;;
     H ) # HTTP(S)
       scanType="http"
-      nmapFlags="${nmapFlags} -p ${httpPorts} --script=http-title.nse \
-        --script=http-headers.nse --script=http-methods.nse \
-        --script=http-apache-server-status.nse --script=http-git.nse \
-        --script=http-ls.nse --script=http-robots.txt.nse \
-        --script=http-userdir-enum.nse \
-        --script http-wordpress-enum --script-args check-latest=true,search-limit=10"
+      nmapFlags="${nmapFlags} -p ${httpPorts} ${httpScripts}"
       ;;
     r ) # max rate
       maxRate=$OPTARG
