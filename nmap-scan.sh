cf466ee896f32640fc3a45baf9c1a68003de7d9e
Pawel <pawel.bartunek@gmail.com>
2019-09-03 06:44:05 +0200

    no ping, additional ports in quick scan

--- nmap-scan.sh
+++ nmap-scan.sh
@@ -3,7 +3,7 @@
 # simple nmap scanning script
 #
 
-quickPorts="21,22,23,80,111,443,445,1080,1098,1099,1198,1199,1414,1433,1521,2375,3306,3389,4443,4444,5555,5800,5900,6000,6379,6600,7001,7002,7202,8000,8009,8080,8081,8042,8082,8088,8090,8089,8181,8282,8443,8888,9000,9001,9043,9090,9443,9700,9494,9797,9800,9875,9999,10090,10099,11098,11099,11111,11112"
+quickPorts="21,22,23,80,111,443,445,1080,1098,1099,1198,1199,1414,1433,1521,1527,2049,2375,3306,3389,4443,4444,5555,5800,5900,6000,6093,6095,6379,6600,7001,7002,7009,7010,7202,8000,8009,8080,8081,8042,8082,8088,8090,8095,8089,8181,8282,8443,8880,8888,8983,9000,9001,9043,9090,9443,9700,9494,9797,9800,9875,9999,10090,10099,11098,11099,11111,11112,12333,24604,24605,60001,61616"
 httpPorts="80,443,3000,5985,8000,8080,8081,8181,8282,8443,8880,9000,9090,9443"
 
 httpScripts="--script=http-title.nse --script=http-userdir-enum.nse \
@@ -18,7 +18,7 @@ scanType=""
 targets=""
 listOfHosts=""
 
-while getopts ":sfqvt:T:Hr:L:" opt; do
+while getopts ":sfqvt:T:Hr:L:P" opt; do
   case ${opt} in
     q ) # quick
       scanType="quick"
@@ -58,6 +58,9 @@ while getopts ":sfqvt:T:Hr:L:" opt; do
       maxRate=$OPTARG
       nmapFlags="${nmapFlags} --max-rate ${maxRate} "
       ;;
+    P ) # no ping
+      nmapFlags="${nmapFlags} -Pn "
+      ;;
     \? ) # help
       echo "Usage: cmd [-q] [-s] [-f] [-v] [-t|-T] [-r]"
       echo "  -q    quick scan - only selected ports"
@@ -68,6 +71,7 @@ while getopts ":sfqvt:T:Hr:L:" opt; do
       echo "  -T    text file with targets to scan"
       echo "  -r    max rate"
       echo "  -H    http scan  - scan common HTTP(s) ports with scripts"
+      echo "  -P    no ping"
       ;;
     : )
       echo "Option: $OPTARG requires an argument"
