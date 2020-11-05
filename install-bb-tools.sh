cf2485cb65c2074d769675f4443948d8ae76fdfc
Paweł <pawel.bartunek@gmail.com>
2020-11-05 12:05:12 +0100

    BugBounty tools install script

---
+++ install-bb-tools.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sudo apt-get update && apt-get dist-upgrade -y
+
+# install tools
+sudo apt-get install -y sudo \
+  git \
+  screen \
+  vim \
+  aptitude \
+  zsh \
+  man \
+  zip \
+  net-tools \
+  wget \
+  ruby \
+  socat  \
+  golang \
+  nmap \
+  chromium-browser \
+  pwgen \
+  jq \
+  tree
+
+# create directories
+directories=(files work bin)
+for dir in ${directories[*]}; do
+  mkdir -p "${HOME}/${dir}"
+done
+
+
+go get -u github.com/tomnomnom/assetfinder \
+github.com/tomnomnom/waybackurls \
+github.com/tomnomnom/meg \
+github.com/tomnomnom/httprobe \
+github.com/tomnomnom/gf \
+github.com/ffuf/ffuf \
+github.com/michenriksen/aquatone \
+github.com/lc/gau \
+github.com/OJ/gobuster \
+github.com/jaeles-project/gospider \
+github.com/projectdiscovery/httpx \
+github.com/projectdiscovery/naabu \
+github.com/projectdiscovery/subfinder
